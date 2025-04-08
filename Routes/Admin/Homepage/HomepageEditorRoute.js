f305438bd5f2307f3fb1f7212737b9e87159b5ea
SharookhR <sharookh.ar20@gmail.com>
2025-04-08 12:25:30 +0530

    fixed error in homepageeditor route

--- Routes/Admin/Homepage/HomepageEditorRoute.js
+++ Routes/Admin/Homepage/HomepageEditorRoute.js
@@ -21,4 +21,16 @@ router.patch('/top-pick/update/:id', verifyToken(['admin']), multerConfig.single
 router.delete('/top-pick/delete/:id', verifyToken(['admin']), HomepageController.deleteTopPickProduct);
 router.get('/top-pick/view', verifyToken(['admin']), HomepageController.getAllTopPickProducts);
 
+// Top Sale Section Routes
+router.post('/top-sale/create', verifyToken(['admin']), multerConfig.single('image'), HomepageController.createTopSaleSection);
+router.patch('/top-sale/update/:id', verifyToken(['admin']), multerConfig.single('image'), HomepageController.updateTopSaleSectionProduct);
+router.delete('/top-sale/delete/:id', verifyToken(['admin']), HomepageController.deleteTopSaleSectionProduct);
+router.get('/top-sale/view', verifyToken(['admin']), HomepageController.getAllTopSaleSectionProducts);
+
+// Referral Section Routes
+router.post('/referral/create', verifyToken(['admin']), multerConfig.single('image'), HomepageController.createReferralSection);
+router.patch('/referral/update/:id', verifyToken(['admin']), multerConfig.single('image'), HomepageController.updateReferralSectionImages);
+router.delete('/referral/delete/:id', verifyToken(['admin']), HomepageController.deleteReferralSectionImages);
+router.get('/referral/view', verifyToken(['admin']), HomepageController.getAllReferralSection);
+
 module.exports = router;
