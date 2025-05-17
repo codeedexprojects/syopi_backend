f741c5e1bf639f200f412a79bcf49d906b3b9026
SharookhR <sharookh.ar20@gmail.com>
2025-05-17 12:50:40 +0530

    Added Detailed view of User

--- Controllers/Admin/User/UserController.js
+++ Controllers/Admin/User/UserController.js
@@ -2,6 +2,8 @@ const User = require('../../../Models/User/UserModel');
 const Product = require('../../../Models/Admin/productModel');
 const Order = require('../../../Models/Vendor/VendorOrderModel');
 const Wishlist = require('../../../Models/User/WishlistModel');
+const Cart =  require('../../../Models/User/cartModel')
+const Review = require('../../../Models/User/ReviewModel')
 
 
 // get all users
@@ -70,6 +72,50 @@ exports.getAllUsers = async (req, res) => {
     }
 };
 
+
+exports.getUserById = async (req, res) => {
+    const userId = req.params.id;
+   
+
+    
+
+    try {
+        // Get User
+        const user = await User.findById(userId).select("-password");
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Get Wishlist 
+        const wishlist = await Wishlist.find({ userId }).populate('productId');
+
+        // Get Cart 
+        const cart = await Cart.findOne({ userId }).populate('items.productId');
+
+        // Get Orders 
+        const orders = await Order.find({ userId }).populate('productId');
+
+        // Total order
+        const totalOrders = await Order.countDocuments({ userId });
+
+        //Get Activity
+        
+        const activity = await Review.find({userId})
+
+        // Response
+        res.status(200).json({
+            user,
+            wishlist,
+            cart,
+            orders,
+            totalOrders,
+            activity
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching user data', error: error.message });
+    }
+};
 
 
 //search users
