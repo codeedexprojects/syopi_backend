bea63583758598925b605dba2eaa20230e5b7140
Sharookh-Codeedex <sharookh.codeedex@gmail.com>
2025-06-05 09:52:58 +0530

    Updated multer upload size

--- Middlewares/MulterConfig.js
+++ Middlewares/MulterConfig.js
@@ -40,7 +40,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 3 * 1024 * 1024 }, // Limit file size to 3MB
+  limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5MB
 });
 
 module.exports = upload;
